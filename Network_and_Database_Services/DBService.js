2f94de91d9650f3edadc33bbd2d3e94521933453
hezbims <hezbisulaiman@gmail.com>
2023-10-18 11:09:33 +0800

    benerin db belum di initialize di awal

--- Network_and_Database_Services/DBService.js
+++ Network_and_Database_Services/DBService.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
 
 let db = null;
-const DBService = async () =>
+const initializeDB = async () =>
     new Promise((resolve , reject) => {
         if (db != null){
             resolve(db);
         }
+
         db = mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -24,4 +25,4 @@ const DBService = async () =>
         });
     });
 
-module.exports = DBService;
+module.exports = initializeDB;
--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const {
   IncrementTransactionController, GetAllModelController, GetDetailModelController,
 } = MainController;
 const createIOTServices = require("./Network_and_Database_Services/IOTServices");
+const initializeDB = require("./Network_and_Database_Services/DBService");
 
 async function initializeServer() {
+  await initializeDB();
   app.use(cors());
   app.use(express.json());
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
 
-
-// ************************************************** API ****************************************************************
   app.get('/api/alldata', GetAllModelController);
   app.get('/api/detail-model/:id', GetDetailModelController);
   app.get('/api/get-model/:id', GetDetailModelController);
